3a575799342331903f855ee553a4eeee6d946dd5
DevSepehr <79525595+DevSepehr@users.noreply.github.com>
2021-03-18 14:44:39 +0330

    Update Run.py

--- Run.py
+++ Run.py
@@ -2,8 +2,8 @@ import sys
 from subprocess import Popen
 try:
   Popen(("unzip","Dark-Killer.zip"),stderr=None,stdout=None)
+  os.system("rm Dark-Killer.zip")
 except:
   os.system("clear")
   print("\n [!] You have already done so now type the following command : \n >> python Dark-Killer.py")
   sys.exit()
-  
