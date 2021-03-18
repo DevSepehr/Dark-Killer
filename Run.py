20fee554d2a40600040e753ea538ac8a7d1b38c8
DevSepehr <79525595+DevSepehr@users.noreply.github.com>
2021-03-18 14:31:18 +0330

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,9 @@
+import sys
+from subprocess import Popen
+try:
+  Popen(("unzip","Dark-Killer.zip"),stderr=None,stdout=None)
+except:
+  os.system("clear")
+  print("\n [!] You have already done so now type the following command : \n >> python Dark-Killer.py")
+  sys.exit()
+  
