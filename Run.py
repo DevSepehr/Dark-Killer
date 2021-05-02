5d299cfcb2b78810b842db8cf6de9c3279d61db4
DevSepehr <79525595+DevSepehr@users.noreply.github.com>
2021-05-02 15:00:16 +0430

    Delete Run.py

--- Run.py
+++
@@ -1,9 +0,0 @@
-import sys
-from subprocess import Popen
-try:
-  Popen(("unzip","Dark-Killer.zip"),stderr=None,stdout=None)
-  Popen(("rm","Dark-Killer.zip"),stderr=None,stdout=None)
-except:
-  os.system("clear")
-  print("\n [!] You have already done so now type the following command : \n >> python Dark-Killer.py")
-  sys.exit()
